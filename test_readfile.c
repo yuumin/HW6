7f2e80781e220b303df767d49e8b3e0af023642b
yuumin <yuko0904k@gmail.com>
2015-07-06 20:26:43 +0900

    Update test_readfile.c

--- test_readfile.c
+++ test_readfile.c
@@ -1,26 +1,60 @@
 #include <stdio.h>
+int BubSort(int x[ ], int n);
+  
+int BubSort(int x[ ], int n)
+{
+    int i, j, temp;
 
-//void read_file(char*fname){
+    for (i = 0; i < n - 1; i++) {
+        for (j = n - 1; j > i; j--) {
+            if (x[j - 1] > x[j]) {  
+                temp = x[j];       
+                x[j] = x[j - 1];
+                x[j - 1]= temp;
+            }
+	}
+    }
+    
+}
+
   int main(void){
-  FILE *fp;
-  char*fname="test1";
-  unsigned char value[100000];
+  FILE *fpr;
+  FILE *fpw;
+  char*fname_r="test1";
+  char*fname_w="result1";
+  int value[2];
   int i,size;
+  int tmp=0;
   
-  fp=fopen(fname,"rb");
+  fpr=fopen(fname_r,"rb");
   
-  if(fp==NULL){
+  if(fpr==NULL){
     printf("cannot open\n");
     return -1;
   }
-  size=fread(value,sizeof(int) ,10000, fp);//kakunousaki yomikomudatahitotunobitesuu yomikomudatanokosuu filepointer
- 
+  size=fread(value,sizeof(int) ,2, fpr);//kakunousaki yomikomudatahitotunobitesuu yomikomudatanokosuu filepointer
+  printf("before sorting\n");
   for(i=0;i<size;i++){
-    if(i%16==0)printf("\n");
-    printf("%02X" , value[i]);
+    printf("%02X\n" , value[i]);
+  }
+  printf("after sorting\n");
+  BubSort(value,size);
+  for(i=0;i<size;i++){
+    printf("%02X\n" , value[i]);
   }
   
-  fclose(fp);
+  
+  
+  fpw=fopen(fname_w,"wb");
+  if(fpw==NULL){
+    printf("cannot open result\n", fname_w);
+    return -1;
+  }
+  
+  fwrite(value,sizeof(int),size,fpw);
+  fclose(fpr);
+  fclose(fpw);
+  
   return 0;
   }
   
